62ab2c7b4e9e65434e4ac0885fd2875668130349
ycek21 <39658861+ycek21@users.noreply.github.com>
2019-12-07 21:52:45 +0100

    Cypher works

--- Steganography/Program.cs
+++ Steganography/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,24 @@ namespace Steganography
     {
         static void Main(string[] args)
         {
+            var steganography = new Steganography();
+            steganography.HideImage("C:/Users/Jacek/Desktop/zdjecie12.png", "ja t");
+            Console.WriteLine(steganography.bm.GetPixel(0, 0));
+            Console.WriteLine(steganography.bm.GetPixel(0, 1));
+            Console.WriteLine(steganography.bm.GetPixel(0, 2));
+            Console.WriteLine(steganography.bm.GetPixel(0, 3));
+            Console.WriteLine(steganography.bm.GetPixel(0, 4));
+            Console.WriteLine(steganography.bm.GetPixel(0, 5));
+
+            Console.WriteLine(steganography.bm.GetPixel(0, 6));
+            Console.WriteLine(steganography.bm.GetPixel(0, 7));
+            Console.WriteLine(steganography.bm.GetPixel(0, 8));
+            Console.WriteLine(steganography.bm.GetPixel(0, 9));
+            Console.WriteLine(steganography.bm.GetPixel(0, 10));
+            Console.WriteLine(steganography.bm.GetPixel(0, 11));
+
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Steganography/Steganography.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steganography
+{
+    public class Steganography
+    {
+        public Bitmap bm;
+
+
+
+        public void HideImage(string filePath, string message)
+        {
+            this.bm = new Bitmap(filePath);
+            Console.WriteLine("Height: " + bm.Height);
+            Console.WriteLine("Width: " + bm.Width);
+
+            var pixelColorIndex = 0;
+            var charIndex = 0;
+            var messageIndex = 0;
+            var currentChar = 0;
+            var finish = false;
+          
+            for (var i = 0; i < bm.Height; i++)
+            {
+                for (var j = 0; j < bm.Width; j++)
+                {
+                    var pixel = bm.GetPixel(i, j);
+
+                    var R = pixel.R - pixel.R % 2;
+                    var G = pixel.G - pixel.G % 2;
+                    var B = pixel.B - pixel.B % 2;
+
+
+                    for (var k = 0; k < 3; k++)
+                    {
+                        if (pixelColorIndex == 0)
+                        {
+                            currentChar = message[0];
+                        }
+
+                        if (pixelColorIndex % 8 == 0)
+                        {
+
+                           
+                            if (pixelColorIndex != 0)
+                            {
+                                messageIndex++;
+
+                                if (messageIndex == message.Length)
+                                {
+                                    bm.Save("result.png", ImageFormat.Png);
+                                    //bm.Dispose();
+                                    return;
+                                }
+                                else
+                                {
+                                    currentChar = message[messageIndex];
+                                }
+
+                            }
+
+                        }
+
+                        switch (pixelColorIndex % 3)
+                        {
+
+                            case 0:
+                            {
+                                R += currentChar % 2;
+                                currentChar /= 2;
+                                pixelColorIndex++;
+                                bm.SetPixel(i, j, Color.FromArgb(R, G, B));
+                                    break;
+                            }
+                            case 1:
+                            {
+                                G += currentChar % 2;
+                                currentChar /= 2;
+                                pixelColorIndex++;
+                                bm.SetPixel(i, j, Color.FromArgb(R, G, B));
+                                    break;
+                            }
+                            case 2:
+                            {
+                                B += currentChar % 2;
+                                currentChar /= 2;
+                                pixelColorIndex++;
+                                bm.SetPixel(i, j, Color.FromArgb(R, G, B));
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            
+
+        }
+        public static int ReverseBits(int n)
+        {
+            int result = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                result = result * 2 + n % 2;
+
+                n /= 2;
+            }
+
+            return result;
+        }
+    }
+}
