c16e9754117c79877805d00c236bf784d749297a
ycek21 <39658861+ycek21@users.noreply.github.com>
2019-12-30 16:29:22 +0100

    HashFunctions init

---
+++ HashFunction/CreateHash.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace HashFunction
+{
+    public class CreateHash
+    {
+        public string CalculateMD5(string message)
+        {
+            using var md5 = MD5.Create();
+            var messageBytes = StringToBytes(message);
+            var encodedMessage = md5.ComputeHash(messageBytes);
+
+            return BytesToString(encodedMessage);
+        }
+
+        private byte[] StringToBytes(string message)
+        {
+            var bytes = Encoding.UTF8.GetBytes(message);
+            return bytes;
+        }
+
+        private string BytesToString(byte[] bytes)
+        {
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                sBuilder.Append(bytes[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+    }
+}
---
+++ HashFunction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var md5 = new CreateHash().CalculateMD5("jacek");
+            Console.WriteLine(md5);
+            Console.ReadLine();
+        }
+    }
+}
