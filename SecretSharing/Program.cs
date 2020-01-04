2743bd9743e2c49c49ce764a4ea63a11f21aa158
ycek21 <39658861+ycek21@users.noreply.github.com>
2020-01-04 16:06:05 +0100

    SecretSharing
    Trivial Method

---
+++ SecretSharing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSharing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var player = new List<int> {1, 2, 3, 4, 5};
+
+            var sum = player.Sum();
+
+            Console.WriteLine(sum);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SecretSharing/TrivialSecretSharing.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SecretSharing
+{
+    public class TrivialSecretSharing
+    {
+        private List<int> _players;
+        private int _secretNumber;
+        private int _numberRange;
+        private Random _random = new Random();
+
+        public void ShareSecret(int numberOfPlayers, int numberRange, int secretNumber)
+        {
+            this._secretNumber = secretNumber;
+            this._numberRange = numberRange;
+
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                if (i == numberOfPlayers - 1)
+                {
+                    _players.Add(_random.Next(0, this._numberRange - 1));
+                }
+                else
+                {
+                    this._players.Add((_secretNumber - this._players.Sum()) % this._numberRange);
+                }
+            }
+        }
+
+        public void RetrieveSecret()
+        {
+
+        }
+    }
+}
